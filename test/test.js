f194cc22809c18b1ac04d0e877a16a16dd895967
zy445566 <zyct@vip.qq.com>
2020-06-08 17:41:38 +0800

    upgrade to Wasm

--- test/test.js
+++ test/test.js
@@ -1,13 +1,17 @@
 const dw = require('../index');
+const path = require('path');
+function getAbsolutePath(fileName) {
+    return path.join(__dirname,fileName)
+}
 //EnCode Image add digital watermarking
-let srcFileName = "srcImg.png";
+let srcFileName = getAbsolutePath("srcImg.png");
 let watermarkText = "github.com/zy445566";
 let fontSize = 1.1;
-let enCodeFileName = "enCode.png";
+let enCodeFileName = getAbsolutePath("enCode.png");
 async function run() {
     await dw.transformImageWithText(srcFileName,watermarkText,fontSize,enCodeFileName);
     //DeCode Image get digital watermarking
-    let deCodeFileName = "deCode.png";
+    let deCodeFileName = getAbsolutePath("deCode.png");
     await dw.getTextFormImage(enCodeFileName,deCodeFileName);
 }
 run()
